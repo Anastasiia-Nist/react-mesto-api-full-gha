62edc377cae451a2df25d1d56e927fcf8706a9c3
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-07-28 17:00:02 +0300

    fix

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,4 +1,5 @@
 const { allowedCors, DEFAULT_ALLOWED_METHODS} = require('../utils/cors')
+
 const corsMiddleware = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
