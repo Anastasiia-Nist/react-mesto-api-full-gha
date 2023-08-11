581448acf6ee52fc6fd67fd130e4110aa1b011d7
Anastasia Nistratova <a.nistratova14@gmail.com>
2023-08-11 17:33:42 +0300

    fix: add localhost cors

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -2,6 +2,7 @@
 const allowedCors = [
   'http://a.nistratova14.fvds.ru',
   'https://a.nistratova14.fvds.ru',
+  'http://localhost:3000',
 ];
 // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
